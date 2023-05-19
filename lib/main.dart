4e000c1f5de441bab5f19a24c2bffeb6aba132fb
Shaimaa Fakhreldin <shaimaa.a.fakhraldin@gmail.com>
2023-05-19 10:50:19 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:notification_example/home.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+AndroidNotificationChannel? channel;
+
+FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
+late FirebaseMessaging messaging;
+
+final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+
+void notificationTapBackground(NotificationResponse notificationResponse) {
+  print('notification(${notificationResponse.id}) action tapped: '
+      '${notificationResponse.actionId} with'
+      ' payload: ${notificationResponse.payload}');
+  if (notificationResponse.input?.isNotEmpty ?? false) {
+    print(
+        'notification action tapped with input: ${notificationResponse.input}');
+  }
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  messaging = FirebaseMessaging.instance;
+
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  //If subscribe based sent notification then use this token
+  final fcmToken = await messaging.getToken();
+  print(fcmToken);
+
+  //If subscribe based on topic then use this
+  await messaging.subscribeToTopic('notification_example');
+
+  // Set the background messaging handler early on, as a named top-level function
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    channel = const AndroidNotificationChannel(
+        'notification_example', // id
+        'notification_example_title', // title
+        importance: Importance.high,
+        enableLights: true,
+        enableVibration: true,
+        showBadge: true,
+        playSound: true);
+
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    final android =
+        AndroidInitializationSettings('@drawable/ic_notifications_icon');
+    final iOS = DarwinInitializationSettings();
+    final initSettings = InitializationSettings(android: android, iOS: iOS);
+
+    await flutterLocalNotificationsPlugin!.initialize(initSettings,
+        onDidReceiveNotificationResponse: notificationTapBackground,
+        onDidReceiveBackgroundNotificationResponse: notificationTapBackground);
+
+    await messaging
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notification',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorKey: navigatorKey,
+      home: HomePage(),
+    );
+  }
+}
